904342095fe498130d12ab6741e5acc9c5bf2857
Kishore <kishorekichu3246@gmail.com>
2025-05-08 19:18:00 +0530

    init(project): Set up initial project structure and configuration
     Initialized project with base folder structure, configurations, and dependencies.

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import app from "./app";
+
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on the PORT ${PORT}`)
+
+})
