c89d27e8ab62e465e83c83c2386b5c0ec45a4aa2
Kishore <kishorekichu3246@gmail.com>
2025-07-06 13:27:44 +0530

    build: Add and change db config for deployment

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,13 +3,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// const pool = new Pool({
+//   user: process.env.POSTGRES_USER,
+//   password: process.env.POSTGRES_PASSWORD,
+//   host: process.env.POSTGRES_HOST,
+//   port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+//   database: process.env.POSTGRES_DB,
+// });
+
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
-  database: process.env.POSTGRES_DB,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 });
+
 
 
 // Function to initialize the database tables
